67c5dadeee4d8bf26f645b9d7e1580b44ff57380
Lucascg1997 <lucascarneirogonzalo@gmail.com>
2021-06-29 12:33:19 +0000

    +

---
+++ app.js
@@ -0,0 +1,115 @@
+let moneyRoberto = prompt("How much money do you have Roberto?");
+let moneyPedro = prompt("How much money do you have Pedro?");
+let moneyPablo = prompt("How much money do you have Pablo?");
+
+moneyRoberto = parseInt(moneyRoberto);
+
+if (moneyRoberto >= 1.6 && moneyRoberto < 1.7) {
+    alert("Buy the apple ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 1.6));
+}
+
+else if (moneyRoberto >= 1.7 && moneyRoberto < 1.8) {
+    alert("Buy the strawberry ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 1.7));
+}
+
+else if (moneyRoberto >= 1.8 && moneyRoberto < 1.9) {
+    alert("Buy the lemon ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 1.8));
+}
+
+else if (moneyRoberto >= 1.9 && moneyRoberto < 2) {
+    alert("Buy the vanilla ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 1.9));
+}
+
+else if (moneyRoberto >= 2 && moneyRoberto < 5) {
+    alert("Buy the chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 2));
+}
+
+else if (moneyRoberto >= 5 && moneyRoberto < 5.8) {
+    alert("Buy the banana split ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 5));
+}
+
+else if (moneyRoberto >= 5 && moneyRoberto < 5.8) {
+    alert("Buy the banana split with chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyRoberto - 5.8));
+}
+
+moneyPedro = parseInt(moneyPedro);
+
+if (moneyPedro >= 1.6 && moneyPedro < 1.7) {
+    alert("Buy the apple ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 1.6));
+}
+
+else if (moneyPedro >= 1.7 && moneyPedro < 1.8) {
+    alert("Buy the strawberry ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 1.7));
+}
+
+else if (moneyPedro >= 1.8 && moneyPedro < 1.9) {
+    alert("Buy the lemon ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 1.8));
+}
+
+else if (moneyPedro >= 1.9 && moneyPedro < 2) {
+    alert("Buy the vanilla ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 1.9));
+}
+
+else if (moneyPedro >= 2 && moneyPedro < 5) {
+    alert("Buy the chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 2));
+}
+
+else if (moneyPedro >= 5 && moneyPedro < 5.8) {
+    alert("Buy the banana split ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 5));
+}
+
+else if (moneyPedro >= 5 && moneyPedro < 5.8) {
+    alert("Buy the banana split with chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPedro - 5.8));
+}
+
+
+moneyPablo = parseInt(moneyPablo);
+
+if (moneyPablo >= 1.6 && moneyPablo < 1.7) {
+    alert("Buy the apple ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 1.6));
+}
+
+else if (moneyPablo >= 1.7 && moneyPablo < 1.8) {
+    alert("Buy the strawberry ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 1.7));
+}
+
+else if (moneyPablo >= 1.8 && moneyPablo < 1.9) {
+    alert("Buy the lemon ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 1.8));
+}
+
+else if (moneyPablo >= 1.9 && moneyPablo < 2) {
+    alert("Buy the vanilla ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 1.9));
+}
+
+else if (moneyPablo >= 2 && moneyPablo < 5) {
+    alert("Buy the chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 2));
+}
+
+else if (moneyPablo >= 5 && moneyPablo < 5.8) {
+    alert("Buy the banana split ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 5));
+}
+
+else if (moneyPablo >= 5 && moneyPablo < 5.8) {
+    alert("Buy the banana split with chocolate ice cream Roberto");
+    alert("you will receive a balance of" + (moneyPablo - 5.8));
+}
